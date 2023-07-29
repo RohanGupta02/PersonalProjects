3c92c27314bdba8fe7831a0b9aa461ab1ebfd847
Rohan Gupta <rogupt@iu.edu>
2023-07-29 13:49:58 -0400

    added some colors and navbar

---
+++ flutter_app/lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static Color white = Colors.white;
+  static Color primaryColor = Color.fromRGBO(19, 41, 61, 1);
+  static Color secondaryColor = Color.fromRGBO(0, 100, 148, 1);
+  static Color tertiaryColor = Color.fromRGBO(36, 123, 160, 1);
+}
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,44 +1,115 @@
-import 'package:english_words/english_words.dart';
+import 'package:circle_nav_bar/circle_nav_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:namer_app/colors.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  //application root
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
+    return MaterialApp(
+      theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          
         ),
-        home: MyHomePage(),
-      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class MyHomePage extends StatelessWidget {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  int _tabIndex = 1;
+  int get tabIndex => _tabIndex;
+  set tabIndex(int v) {
+    _tabIndex = v;
+    setState(() {});
+  }
+
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: _tabIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
     return Scaffold(
-      body: Column(
+      extendBody: true,
+      bottomNavigationBar: CircleNavBar(
+        activeIcons:  [
+          Icon(Icons.person,color: AppColors.tertiaryColor,),
+          Icon(Icons.home, color: AppColors.tertiaryColor),
+          Icon(Icons.favorite, color: AppColors.tertiaryColor),
+        ],
+        inactiveIcons: [
+          Text(
+            "Page1",
+            style: TextStyle(
+              color: AppColors.tertiaryColor,
+              fontSize: 25,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          Text(
+            "Page2",
+            style: TextStyle(
+              color: AppColors.tertiaryColor,
+              fontSize: 25,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          Text(
+            "Page3",
+            style: TextStyle(
+              color: AppColors.tertiaryColor,
+              fontSize: 25,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ],
+        color: AppColors.primaryColor,
+        height: 60,
+        circleWidth: 60,
+        activeIndex: tabIndex,
+        onTap: (index) {
+          tabIndex = index;
+          pageController.jumpToPage(tabIndex);
+        },
+        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
+        cornerRadius: const BorderRadius.only(
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+          bottomRight: Radius.circular(24),
+          bottomLeft: Radius.circular(24),
+        ),
+        shadowColor: AppColors.tertiaryColor,
+        elevation: 10,
+      ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (v) {
+          tabIndex = v;
+        },
         children: [
-          Text('A random:'),
-          Text(appState.current.asLowerCase),
+          Container(width: double.infinity, height: double.infinity, color: AppColors.white),
+          Container(width: double.infinity, height: double.infinity, color: AppColors.white),
+          Container(width: double.infinity, height: double.infinity, color: AppColors.white),
         ],
       ),
     );
